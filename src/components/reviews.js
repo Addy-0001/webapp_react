2705e7d47789451e1f1180b2f318670249468cc5
Addy-0001 <adamsneupane@gmail.com>
2022-11-10 21:47:25 +0545

    added reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -2,7 +2,48 @@ import "./reviews.css";
 
 function reviews()
 {
-    
+    return(
+        <div className="reviews">
+
+      <div className="manoj">
+        <br />
+        Training with Adamya was the best thing I could have done for my fitness
+        goals. Over the course of 3 months, He's helped me lose more than 10
+        kilos and in such a way that it's sustainable. For anyone looking for
+        guidance, I'd highly recommend Adamya. He's a good Trainer.
+        <br />
+        <small>
+          -Manoj Adhikari, <br />
+          &nbsp; Student, Australia
+        </small>
+      </div>
+
+      <div className="dipjung">
+        Adamya has been a vital part of keeping me consistent with the exercises
+        I do. His method of training is in line with behavior change and he
+        makes dynamic training plans and programs depending on how my lifestyle
+        changes. He constantly pushes me and checks in very often to ensure that
+        I'm doing what we agreed on. I enjoy having him as my trainer and it's
+        changed how I approach exercises be it at home or at the gym
+        <br />
+        <small>
+          -Dip Jung Thapa, <br />
+          &nbsp; COO, Kratikal, India
+        </small>
+      </div>
+
+      <div className="nishant">
+        I got see visible effective results on my body in a short period of time
+        by training with Adamya. His way of providing training by pushing,
+        motivating and controlling daily diet seems to be very effective.
+        <br />
+        <small>
+          -Nishant Khadka, <br />
+          &nbsp; Student, Nepal
+        </small>
+      </div>
+        </div>
+    ); 
 }
 
 export default reviews;
