348a0ee609c10a3665b22e9c9662765522f61b32
Addy-0001 <adamsneupane@gmail.com>
2022-11-10 21:40:52 +0545

    added about me section

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,28 @@ import "./about.css";
 
 function about()
 {
-    
+    return (
+      <div className="about-container">
+        <section className="AlbertBox">
+          <h3>A little about me.</h3>
+          <p>
+            I started training in around February of 2018. I struggled a lot
+            with my weight loss journey because of a lack of good training and
+            nutrition knowledge. It took me months and months of trying to
+            understand the human body and exercise physiology to make a
+            sustainable diet and training routine and stick to it. This took
+            learning about behavior change as well. After I was able to make a
+            good training routine, I started helping out friends who saw rapid
+            changes unlike me because of good guidance that I didn't have.
+            That's why I decided to start this business and help people gain the
+            knowledge necessary to potentially
+            <em>
+              <b> change their lives.</b>
+            </em>
+          </p>
+        </section>
+      </div>
+    );
 }
 
 export default about;
