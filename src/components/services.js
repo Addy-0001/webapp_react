0567f755bff8e85e8229ea48676feb395eb4d173
Addy-0001 <adamsneupane@gmail.com>
2022-11-10 21:43:36 +0545

    added services section

--- src/components/services.js
+++ src/components/services.js
@@ -2,7 +2,18 @@ import "./services.css";
 
 function services()
 {
-    
+    return (
+      <div className="plans">
+        <h3>I do Amazing Things for Clients</h3>
+        <h2>Fitness Training</h2>
+        <br />
+        <small>I can make custom fitness plans for you.</small> <br />
+        <br />
+        <h2>Diet Planning</h2>
+        <small>I can also make custom diet plans for you.</small> <br />
+        <br />
+      </div>
+    );
 }
 
 export default services;
