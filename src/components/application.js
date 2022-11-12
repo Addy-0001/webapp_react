f588591cb30fd341bc7b6b9a617380d57e143a97
Addy-0001 <adamsneupane@gmail.com>
2022-11-12 16:13:45 +0545

    added application and certificate

--- src/components/application.js
+++ src/components/application.js
@@ -2,7 +2,16 @@ import "./application.css";
 
 function application()
 {
-    
+    return(
+        <div>
+        <h2> Do you want to achieve your <em> Dream Physique? </em></h2>
+        <a href="https://forms.gle/S96QCmJSuo4km8DV9" target="_blank">
+        <button class="form-button">
+          <h2> Apply Here</h2>
+          </button>
+        </a>
+        </div>
+    );
 }
 
 export default application;
--- src/components/certificates.js
+++ src/components/certificates.js
@@ -1,8 +1,14 @@
 import "./certificate.css";
+import certificate from "../Images/Certificate_adamya.jpg"
 
 function certificates()
 {
-    
+    return (
+      <div className="certification">
+        <h3>My Certification</h3>
+        <img src={certificate} alt="Certification-Adamya-Neupane" />
+      </div>
+    );
 }
 
 export default certificates;
