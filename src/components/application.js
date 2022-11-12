ff75ac09a685cffe648d9824354d0993c3f07dc6
Addy-0001 <adamsneupane@gmail.com>
2022-11-12 17:08:45 +0545

    completed application

--- src/components/application.js
+++ src/components/application.js
@@ -3,10 +3,10 @@ import "./application.css";
 function application()
 {
     return(
-        <div>
+        <div className="GoogleForm">
         <h2> Do you want to achieve your <em> Dream Physique? </em></h2>
         <a href="https://forms.gle/S96QCmJSuo4km8DV9" target="_blank">
-        <button class="form-button">
+        <button className="form-button">
           <h2> Apply Here</h2>
           </button>
         </a>
